e35798e87bfa5473342578320b0b888214d29a5f
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-04 17:44:54 -0500

    add axios2 global instance

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useEffect } from 'react';
 
 const productsUrl = 'https://course-api.com/react-store-products';
@@ -5,13 +6,19 @@ const randomUserUrl = 'https://randomuser.me/api';
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try {
+      const resp1 = await axios(productsUrl);
+      const resp2 = await axios(randomUserUrl);
+      console.log(resp1, resp2);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>global instance</h2>;
+  return <h2 className="text-center">global instance</h2>;
 };
 export default GlobalInstance;
