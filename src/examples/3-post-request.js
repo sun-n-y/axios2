87c2c4bc91eb884fcc15ba38b3a74799aae990ac
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-04 17:22:11 -0500

    add axios2 post request

--- src/examples/3-post-request.js
+++ src/examples/3-post-request.js
@@ -8,38 +8,43 @@ const PostRequest = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(name, email);
+    try {
+      const resp = await axios.post(url, { name, email });
+      console.log(resp);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <section>
-      <h2 className='text-center'>post request</h2>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+      <h2 className="text-center">post request</h2>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
           <input
-            type='text'
-            className='form-input'
-            id='name'
+            type="text"
+            className="form-input"
+            id="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
         </div>
-        <div className='form-row'>
-          <label htmlFor='email' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
             email
           </label>
           <input
-            type='email'
-            className='form-input'
-            id='email'
+            type="email"
+            className="form-input"
+            id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           login
         </button>
       </form>
